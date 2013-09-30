b9a470b140abd619dc499564ac5c321a24e22f16
Eric Young <eric.j.young88@gmail.com>
2013-09-30 15:39:52 -0600

    Add java file for CharacterFragment

---
+++ Code/src/edu/mines/alterego/CharacterFragment.java
@@ -0,0 +1,15 @@
+package edu.mines.alterego;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class CharacterFragment extends FragmentActivity {
+
+    @override
+    public void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        //TODO XML setContentView(R.layout.<xml here>)
+    }
+
+}
