ca548faf34551249be23a59aa2f37e30c5e80f34
Matt Buland <gik0geck0@gmail.com>
2013-10-01 23:54:31 -0600

    Include documentation statement, and MIT Liscense

--- Code/src/edu/mines/alterego/MainActivity.java
+++ Code/src/edu/mines/alterego/MainActivity.java
@@ -27,6 +27,44 @@ import edu.mines.alterego.CharacterDBHelper;
 import edu.mines.alterego.GameData;
 import edu.mines.alterego.GameActivity;
 
+/**
+ *  Alter Ego
+ *  @author: Matt Buland, Maria Deslis, Eric Young
+ *
+ * ----------------------------------------------------------------------------
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Matt Buland, Maria Deslis, Eric Young
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * ----------------------------------------------------------------------------
+ *
+ * @version: 0.1
+ *
+ * Release Notes:
+ *
+ * 0.1:
+ *      The basic functionality is *basically* there. Games and characters can
+ *      be created. The remaining components will follow the same flow: have a
+ *      display; click button to add things to "display" (from database); use
+ *      a dialog to get input-parameters.
+ */
 public class MainActivity extends Activity implements View.OnClickListener, ListView.OnItemClickListener {
 
     ArrayAdapter<GameData> mGameDbAdapter;
