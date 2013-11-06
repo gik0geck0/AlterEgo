74f8b3122d54b0470cc14ec8d9f1ccc0631ab791
Matt Buland <gik0geck0@gmail.com>
2013-11-06 15:31:31 -0700

    Map working, YAY!

---
+++ AlterEgo/src/edu/mines/alterego/MapActivity.java
@@ -0,0 +1,14 @@
+package edu.mines.alterego;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mapactivity);
+    }
+}
+
