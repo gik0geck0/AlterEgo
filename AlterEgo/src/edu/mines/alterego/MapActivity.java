b1c6b48c9cfbe4affd60e416eedbdb1da3c74407
Maria Deslis <mdeslis@mines.edu>
2013-11-25 11:47:00 -0700

    creating new dialog for long click on map; currently suffering nullpointerexception when clicking on choice

--- AlterEgo/src/edu/mines/alterego/MapActivity.java
+++ AlterEgo/src/edu/mines/alterego/MapActivity.java
@@ -5,9 +5,12 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,10 @@ public class MapActivity extends Activity {
 	GoogleMap map;
 	Context context = this;
 	int count = 0;
+	
+	private RadioGroup markerGroup;
+	private RadioButton markerButton;
+	int markerId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,79 +46,51 @@ public class MapActivity extends Activity {
 
 			@Override
 			public void onMapLongClick(final LatLng position) {
-				LayoutInflater li = LayoutInflater.from(context);
-				final View v = li.inflate(R.layout.new_marker_dialog, null);
+				Log.i("AlterEgos::MapAct::onMapLogClick", "Long Click Occurred on Map");
+				
+				LayoutInflater mapLayI = LayoutInflater.from(context);
+				final View mapV = mapLayI.inflate(R.layout.new_marker_dialog, null);
 				AlertDialog.Builder addMarker = new AlertDialog.Builder(context);
-				addMarker.setView(v);
+				
+				markerGroup = (RadioGroup) findViewById(R.id.add_marker_group);
+				
+				addMarker.setView(mapV);
 				addMarker.setTitle("Add Marker");
-				addMarker.setCancelable(false);
-				addMarker.setPositiveButton("Create",
-						new DialogInterface.OnClickListener() {
-
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// grab user input for marker name
-								EditText mTitle = (EditText) v
-										.findViewById(R.id.marker_title);
-
-								float mColor;
-
-								
-								switch(count) {
-									case 0:
-										mColor = BitmapDescriptorFactory.HUE_RED;
-										break;
-									case 1:
-										mColor = BitmapDescriptorFactory.HUE_ORANGE;
-										break;
-									case 2:
-										mColor = BitmapDescriptorFactory.HUE_YELLOW;
-										break;
-									case 3:
-										mColor = BitmapDescriptorFactory.HUE_GREEN;
-										break;
-									case 4:
-										mColor = BitmapDescriptorFactory.HUE_BLUE;
-										break;
-									case 5:
-										mColor = BitmapDescriptorFactory.HUE_CYAN;
-										break;
-									case 6:
-										mColor = BitmapDescriptorFactory.HUE_VIOLET;
-										break;
-									case 7:
-										mColor = BitmapDescriptorFactory.HUE_MAGENTA;
-										break;
-									case 8:
-										mColor = BitmapDescriptorFactory.HUE_AZURE;
-										break;
-									case 9:
-										mColor = BitmapDescriptorFactory.HUE_ROSE;
-										count = -1;
-										break;
-									default:
-										mColor = BitmapDescriptorFactory.HUE_RED;
-										break;
-								}		
-							
-
-								if (mTitle.getText().toString().equals("")) {
-									Toast name = Toast.makeText(MapActivity.this, "Required: Name", Toast.LENGTH_SHORT);
-									name.show();
-								} else {
-									map.addMarker(new MarkerOptions()
-											.title(mTitle.getText().toString())
-											.position(position)
-											.icon(BitmapDescriptorFactory
-													.defaultMarker(mColor))
-											.draggable(true));
-									count++;
-								}
-							}
-
-						});
-
+				addMarker.setCancelable(true);
+				addMarker.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Log.i("AlterEgos::MapAct::onMapLogClick::onClick", "AlertDialog Created");
+						
+					    // get selected radio button from radioGroup
+						markerId = markerGroup.getCheckedRadioButtonId();
+						
+						Log.i("AlterEgos::MapAct::onMapLogClick::onClick::markerId::", " " + markerId);
+						
+						// find the radio button by returned id
+					    markerButton = (RadioButton) findViewById(markerId);
+			 
+						Toast.makeText(MapActivity.this,
+							markerButton.getText(), Toast.LENGTH_SHORT).show();
+						
+//						switch (markerId) {
+//							case R.id.add_marker_player:
+//								addPlayer(position);
+//								break;
+//							case R.id.add_marker_treasure:
+//								addTreasure(position);
+//								break;
+//							case R.id.add_marker_enemy:
+//								addEnemy(position);
+//								break;
+//							default:
+//								Toast.makeText(MapActivity.this, "Nothing Selected", Toast.LENGTH_SHORT).show();
+//								break;
+//						}
+					}
+				});
+				
 				addMarker.setNegativeButton("Cancel",
 						new DialogInterface.OnClickListener() {
 
@@ -123,11 +102,111 @@ public class MapActivity extends Activity {
 							}
 						});
 
-				AlertDialog alert = addMarker.create();
-				alert.show();
+				AlertDialog markerAlert = addMarker.create();
+				markerAlert.show();
 
+				
 			}
 		});
 
+	}
+	
+	public void addPlayer(final LatLng position) {
+
+		LayoutInflater mLayI = LayoutInflater.from(context);
+		final View mV = mLayI.inflate(R.layout.new_player_marker_dialog, null);
+		AlertDialog.Builder addPlayer = new AlertDialog.Builder(context);
+		addPlayer.setView(mV);
+		addPlayer.setTitle("Create Player");
+		addPlayer.setCancelable(true);
+		addPlayer.setPositiveButton("Create",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog,
+							int which) {
+						// grab user input for marker name
+						EditText mTitle = (EditText) mV
+								.findViewById(R.id.marker_player_title);
+
+						float mColor;
+
+						
+						switch(count) {
+							case 0:
+								mColor = BitmapDescriptorFactory.HUE_RED;
+								break;
+							case 1:
+								mColor = BitmapDescriptorFactory.HUE_ORANGE;
+								break;
+							case 2:
+								mColor = BitmapDescriptorFactory.HUE_YELLOW;
+								break;
+							case 3:
+								mColor = BitmapDescriptorFactory.HUE_GREEN;
+								break;
+							case 4:
+								mColor = BitmapDescriptorFactory.HUE_BLUE;
+								break;
+							case 5:
+								mColor = BitmapDescriptorFactory.HUE_CYAN;
+								break;
+							case 6:
+								mColor = BitmapDescriptorFactory.HUE_VIOLET;
+								break;
+							case 7:
+								mColor = BitmapDescriptorFactory.HUE_MAGENTA;
+								break;
+							case 8:
+								mColor = BitmapDescriptorFactory.HUE_AZURE;
+								break;
+							case 9:
+								mColor = BitmapDescriptorFactory.HUE_ROSE;
+								count = -1;
+								break;
+							default:
+								mColor = BitmapDescriptorFactory.HUE_RED;
+								break;
+						}		
+					
+
+						if (mTitle.getText().toString().equals("")) {
+							Toast name = Toast.makeText(MapActivity.this, "Required: Name", Toast.LENGTH_SHORT);
+							name.show();
+						} else {
+							map.addMarker(new MarkerOptions()
+									.title(mTitle.getText().toString())
+									.position(position)
+									.icon(BitmapDescriptorFactory
+											.defaultMarker(mColor))
+									.draggable(true));
+							count++;
+						}
+					}
+
+				});
+
+		addPlayer.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog,
+							int which) {
+						dialog.cancel();
+
+					}
+				});
+
+		AlertDialog alert = addPlayer.create();
+		alert.show();
+
+	}
+	
+	public void addTreasure(final LatLng position) {
+		//TO DO
+	}
+	
+	public void addEnemy(final LatLng position) {
+		//TO DO
 	}
 }
