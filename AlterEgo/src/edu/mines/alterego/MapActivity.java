bf48d630e9b3978d800bc6e815968a45c7cd432e
Maria Deslis <mdeslis@mines.edu>
2013-11-24 21:14:08 -0700

    made alert dialog for adding a marker have a requirement so that player name cannot be null

--- AlterEgo/src/edu/mines/alterego/MapActivity.java
+++ AlterEgo/src/edu/mines/alterego/MapActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -18,27 +21,27 @@ import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 
 public class MapActivity extends Activity {
-	
-	TileOverlay overlay;
+
 	GoogleMap map;
-	Context context = this ;
+	Context context = this;
 	int count = 0;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mapactivity);
 
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        map.setMapType(GoogleMap.MAP_TYPE_NONE);
-        TileOverlayOptions opts = new TileOverlayOptions();
-        opts.tileProvider(new CustomMapTileProvider(getAssets()));
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mapactivity);
 
-        overlay = map.addTileOverlay(opts);
- 
-        // Creating onLongClickListener for user to add marker to map      
-        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-			
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		map.setMapType(GoogleMap.MAP_TYPE_NONE);
+		TileOverlayOptions opts = new TileOverlayOptions();
+		opts.tileProvider(new CustomMapTileProvider(getAssets()));
+
+		TileOverlay overlay = map.addTileOverlay(opts);
+
+		// Creating onLongClickListener for user to add marker to map
+		map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+
 			@Override
 			public void onMapLongClick(final LatLng position) {
 				LayoutInflater li = LayoutInflater.from(context);
@@ -47,68 +50,75 @@ public class MapActivity extends Activity {
 				addMarker.setView(v);
 				addMarker.setTitle("Add Marker");
 				addMarker.setCancelable(false);
-				addMarker.setPositiveButton("Create", new DialogInterface.OnClickListener() {
-				
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						//grab user input for marker name
-						EditText mTitle = (EditText) v.findViewById(R.id.marker_title);
-						float mColor;
-						
-						if (count == 0) {
-							mColor = BitmapDescriptorFactory.HUE_RED;
-						} else if (count == 1 ) {
-							mColor = BitmapDescriptorFactory.HUE_ORANGE;
-						} else if (count == 2) {
-							mColor = BitmapDescriptorFactory.HUE_YELLOW;
-						} else if (count == 3) {
-							mColor = BitmapDescriptorFactory.HUE_GREEN;
-						} else if (count == 4) {
-							mColor = BitmapDescriptorFactory.HUE_BLUE;
-						} else if (count == 5) {
-							mColor = BitmapDescriptorFactory.HUE_VIOLET;
-						} else if (count == 6) {
-							mColor = BitmapDescriptorFactory.HUE_AZURE;
-						} else if (count == 7) {
-							mColor = BitmapDescriptorFactory.HUE_MAGENTA;
-						} else if (count == 8) {
-							mColor = BitmapDescriptorFactory.HUE_ROSE;
-						} else if (count == 9) {
-							mColor = BitmapDescriptorFactory.HUE_CYAN;
-						} else {
-							mColor = BitmapDescriptorFactory.HUE_YELLOW;
-						}
-					
-						map.addMarker(new MarkerOptions()
-								.title(mTitle.getText().toString())
-								.position(position)
-								.icon(BitmapDescriptorFactory.defaultMarker(mColor))
-								.draggable(true)
-								);
-						
-						if (mColor > 9) {
-							count = 0;
-						} else {
-							count++;
-						}
-					}
-				
-				});
-				
-				addMarker.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.cancel();
-						
-					}
-				});
-				
+				addMarker.setPositiveButton("Create",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								// grab user input for marker name
+								EditText mTitle = (EditText) v
+										.findViewById(R.id.marker_title);
+
+								float mColor;
+
+								if (count == 0) {
+									mColor = BitmapDescriptorFactory.HUE_RED;
+								} else if (count == 1) {
+									mColor = BitmapDescriptorFactory.HUE_ORANGE;
+								} else if (count == 2) {
+									mColor = BitmapDescriptorFactory.HUE_YELLOW;
+								} else if (count == 3) {
+									mColor = BitmapDescriptorFactory.HUE_GREEN;
+								} else if (count == 4) {
+									mColor = BitmapDescriptorFactory.HUE_BLUE;
+								} else if (count == 5) {
+									mColor = BitmapDescriptorFactory.HUE_VIOLET;
+								} else if (count == 6) {
+									mColor = BitmapDescriptorFactory.HUE_AZURE;
+								} else if (count == 7) {
+									mColor = BitmapDescriptorFactory.HUE_MAGENTA;
+								} else if (count == 8) {
+									mColor = BitmapDescriptorFactory.HUE_ROSE;
+								} else if (count == 9) {
+									mColor = BitmapDescriptorFactory.HUE_CYAN;
+									count = 0;
+								} else {
+									mColor = BitmapDescriptorFactory.HUE_YELLOW;
+								}
+
+								if (mTitle.getText().toString().equals("")) {
+									Toast name = Toast.makeText(MapActivity.this, "Please fill player name", Toast.LENGTH_SHORT);
+									name.show();
+								} else {
+									map.addMarker(new MarkerOptions()
+											.title(mTitle.getText().toString())
+											.position(position)
+											.icon(BitmapDescriptorFactory
+													.defaultMarker(mColor))
+											.draggable(true));
+									count++;
+								}
+							}
+
+						});
+
+				addMarker.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								dialog.cancel();
+
+							}
+						});
+
 				AlertDialog alert = addMarker.create();
 				alert.show();
-				
+
 			}
 		});
-    }
-}
 
+	}
+}
